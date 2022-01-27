059b0b6ca98fb0c64f9249f52e32f80815b7bc59
andres-vizcaino <andres.vizcaino@igloo-lab.com>
2022-01-26 19:46:07 -0500

    ✨ View directory

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Tweet from 'components/Tweet'
 import { ITweet } from 'models/Tweet.model'
 import type { NextPage } from 'next'
 import { useSession } from 'next-auth/react'
+import Link from 'next/link'
 import useSWR from 'swr'
 
 const Home: NextPage = () => {
@@ -15,7 +16,17 @@ const Home: NextPage = () => {
         <p className="text-center text-4xl">No estás conectado. 🥲</p>
       ) : (
         <>
-          <CreateTweet />
+          <div className="flex justify-center gap-1 sm:gap-10 flex-col sm:flex-row items-center">
+            <CreateTweet />
+            <Link href={'/profile'}>
+              <a
+                type="button"
+                className="mt-4 max-w-lg bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              >
+                Ver directorio 👀
+              </a>
+            </Link>
+          </div>
           <div className="mt-10 flex flex-col gap-7 justify-center items-center">
             <div className="text-red-500 font-medium">
               Chismosea los ultimos estados
