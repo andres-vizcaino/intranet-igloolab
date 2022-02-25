9e346ec734209eaf822817fa06ef82d30200c787
andres-vizcaino <andres.vizcaino@igloo-lab.com>
2022-02-25 10:29:49 -0500

    🐛  Bug fix (not show menu if not session)

--- components/MenuButtons.tsx
+++ components/MenuButtons.tsx
@@ -1,45 +1,52 @@
+import { useSession } from 'next-auth/react'
 import Link from 'next/link'
 import CreateTweet from './CreateTweet'
 
-const MenuButtons = () => (
-  <div className="flex justify-center gap-1 sm:gap-10 flex-col sm:flex-row items-center">
-    <CreateTweet />
-    <Link href={'/profile'}>
-      <a
-        type="button"
-        className="mt-4 max-w-lg bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-      >
-        Ver directorio 👀
-      </a>
-    </Link>
+const MenuButtons = () => {
+  const { data: session, status } = useSession()
 
-    <Link href={'/pets'}>
-      <a
-        type="button"
-        className="mt-4 max-w-lg bg-cyan-500 hover:bg-cyan-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-      >
-        Ver mascotas 🐶
-      </a>
-    </Link>
+  if (status === 'unauthenticated' || session == null) return null
 
-    <Link href={'/blog'}>
-      <a
-        type="button"
-        className="mt-4 max-w-lg bg-violet-500 hover:bg-violet-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-      >
-        Ir al Blog 🍕
-      </a>
-    </Link>
+  return (
+    <div className="flex justify-center gap-1 sm:gap-10 flex-col sm:flex-row items-center">
+      <CreateTweet />
+      <Link href={'/profile'}>
+        <a
+          type="button"
+          className="mt-4 max-w-lg bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        >
+          Ver directorio 👀
+        </a>
+      </Link>
 
-    <Link href={'/boards'}>
-      <a
-        type="button"
-        className="mt-4 max-w-lg bg-cyan-500 hover:bg-cyan-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-      >
-        Ir tus tableros 📋
-      </a>
-    </Link>
-  </div>
-)
+      <Link href={'/pets'}>
+        <a
+          type="button"
+          className="mt-4 max-w-lg bg-cyan-500 hover:bg-cyan-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        >
+          Ver mascotas 🐶
+        </a>
+      </Link>
+
+      <Link href={'/blog'}>
+        <a
+          type="button"
+          className="mt-4 max-w-lg bg-violet-500 hover:bg-violet-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        >
+          Ir al Blog 🍕
+        </a>
+      </Link>
+
+      <Link href={'/boards'}>
+        <a
+          type="button"
+          className="mt-4 max-w-lg bg-cyan-500 hover:bg-cyan-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        >
+          Ir tus tableros 📋
+        </a>
+      </Link>
+    </div>
+  )
+}
 
 export default MenuButtons
