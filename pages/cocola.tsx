0ab3b615b3554de47c85cecfeaba7d2d730439c3
dev-igloolab <andres.vizcaino@igloolab.co>
2023-03-02 11:43:23 -0500

    ✨ add formato

--- pages/cocola.tsx
+++ pages/cocola.tsx
@@ -1,6 +1,19 @@
 import Image from 'next/image'
 import { NextApplicationPage } from './_app'
 
+const DOWNLOADS = [
+  {
+    title: '¿Sabes que es el acoso laboral?',
+    url: '/pdf/que-es-acoso-laboral.pdf',
+    image: '/img/que-es-acoso-laboral.png',
+  },
+  {
+    title: 'Formato de quejas de acoso laboral',
+    url: '/docx/formato-quejas-posible-acoso-laboral.docx',
+    image: '/img/que-es-acoso-laboral.png',
+  },
+]
+
 const CocolaPage: NextApplicationPage = () => (
   <div className="text-center mt-5 flex flex-col items-center">
     <div className="max-w-4xl">
@@ -38,35 +51,39 @@ const CocolaPage: NextApplicationPage = () => (
 
         <section>
           <h4 className="text-xl my-10 underline">Descargables</h4>
-
-          <div className="shadow-lg rounded-2xl bg-white p-4">
-            <div className="flex-row gap-4 flex justify-center items-center">
-              <div className="flex-shrink-0">
-                <a href="#" className="relative block">
-                  <Image
-                    alt="profil"
-                    src="/img/que-es-acoso-laboral.png"
-                    className="mx-auto object-cover rounded-full h-16 w-16"
-                    objectFit="cover"
-                    width={64}
-                    height={64}
-                  />
+          {DOWNLOADS.map((download, index) => (
+            <div
+              key={index}
+              className="shadow-lg rounded-2xl my-2 bg-white p-4"
+            >
+              <div className="flex-row gap-4 flex justify-center items-center">
+                <div className="flex-shrink-0">
+                  <a href="#" className="relative block">
+                    <Image
+                      alt="profil"
+                      src={download.image}
+                      className="mx-auto object-cover rounded-full h-16 w-16"
+                      objectFit="cover"
+                      width={64}
+                      height={64}
+                    />
+                  </a>
+                </div>
+                <div className="flex flex-col w-full">
+                  <span className="text-lg font-medium text-gray-600">
+                    {download.title}
+                  </span>
+                </div>
+                <a
+                  download
+                  href={download.url}
+                  className="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white  transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg"
+                >
+                  Descargar
                 </a>
               </div>
-              <div className="flex flex-col w-full">
-                <span className="text-lg font-medium text-gray-600">
-                  ¿Sabes que es el acoso laboral?
-                </span>
-              </div>
-              <a
-                download
-                href="/pdf/que-es-acoso-laboral.pdf"
-                className="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white  transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg"
-              >
-                Descargar PDF
-              </a>
             </div>
-          </div>
+          ))}
         </section>
       </div>
     </div>
