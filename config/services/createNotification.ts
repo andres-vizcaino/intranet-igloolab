1f73a9b851431c7e8ec9562dc052e4f150d27a7a
dev-igloolab <andres.vizcaino@igloolab.co>
2023-03-03 09:48:59 -0500

    update url bot

--- config/services/createNotification.ts
+++ config/services/createNotification.ts
@@ -1,8 +1,8 @@
-const URL_CHAT_SPACE_MIGRACION =
-  'https://chat.googleapis.com/v1/spaces/AAAA2AFHCDA/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=R9OdaGu_skX5fwNEIAgDHMGk5rHOTGlb-hzxCLaDaKI%3D'
+const URL_CHAT_SPACE_ANUNCIOS =
+  'https://chat.googleapis.com/v1/spaces/AAAAF6JOW0A/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=JH6GM8sHrsmFpBTp-bwMuWyi6LXXS5sGYHa9Gpiy4Bs%3D'
 
 export const createNotification = async ({ text }: { text: string }) => {
-  await fetch(URL_CHAT_SPACE_MIGRACION, {
+  await fetch(URL_CHAT_SPACE_ANUNCIOS, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json; charset=utf-8',
