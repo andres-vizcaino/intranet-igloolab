fbf577b75521475980e4d7f0ed7fc27ef949eef9
Andrés Vizcaíno <andres.vizcaino@igloolab.co>
2023-06-07 13:15:34 -0500

    add cors

--- pages/api/users/[id].ts
+++ pages/api/users/[id].ts
@@ -1,6 +1,7 @@
 import { User } from '@prisma/client'
 import { NextApiRequest, NextApiResponse } from 'next'
 import prisma from 'lib/prisma'
+import NextCors from 'nextjs-cors'
 
 type Data = User | null
 
@@ -8,6 +9,15 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  // Run the cors middleware
+  // nextjs-cors uses the cors package, so we invite you to check the documentation https://github.com/expressjs/cors
+  await NextCors(req, res, {
+    // Options
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    origin: '*',
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  })
+
   const { id } = req.query
 
   const user = await prisma.user.findUnique({
--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -1,6 +1,7 @@
 import { Profile } from '@prisma/client'
 import { NextApiRequest, NextApiResponse } from 'next'
 import prisma from 'lib/prisma'
+import NextCors from 'nextjs-cors'
 
 type Data =
   | {
@@ -15,6 +16,15 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  // Run the cors middleware
+  // nextjs-cors uses the cors package, so we invite you to check the documentation https://github.com/expressjs/cors
+  await NextCors(req, res, {
+    // Options
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    origin: '*',
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  })
+
   const user = await prisma.user.findMany({
     select: {
       id: true,
