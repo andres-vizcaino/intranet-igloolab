0fd9da824a08410ba8c84ca2bacb9d3c3fc8f4d4
andres.vizcaino <andres.vizcaino@igloolab.co>
2023-08-02 08:53:51 -0500

    upload icon pineapple

--- pages/maintenance.tsx
+++ pages/maintenance.tsx
@@ -1,6 +1,13 @@
+import PineappleMaintenance from 'components/PineappleMaintenance'
+
 const MaintenancePage = () => (
-  <div className="grid place-content-center min-h-screen">
+  <div className="grid place-content-center place-items-center min-h-screen">
+    <PineappleMaintenance />
     <h1 className="text-6xl font-semibold">intranet en mantenimiento 🛠️</h1>
+    <p className="text-lg">
+      Nuestro sitio web está en construcción, estamos trabajando muy duro para
+      brindarte la mejor experiencia.
+    </p>
   </div>
 )
 export default MaintenancePage
